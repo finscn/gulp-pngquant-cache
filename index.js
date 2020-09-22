82689ae570bb306cd822f817fcbf3516789fa11b
finscn <finscn@gmail.com>
2020-09-22 17:46:52 +0800

    use sync spawn & update test-case

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// const spawnSync = require('child_process').spawnSync;
+const spawn = require('child_process').spawn;
 const through = require('through2');
+const pngquant = require('pngquant-bin');
+
 const log = require('fancy-log');
 const PluginError = require('plugin-error');
-const pngquant = require('pngquant-bin');
-const spawnSync = require('child_process').spawnSync;
 
 const packageConfig = require('./package.json');
 const PLUGIN_NAME = packageConfig.name;
@@ -17,7 +19,9 @@ function gulpPngquant(options, customPngquantPath) {
         cache = require('./cache');
         // 自定义缓存
         if (options.cachePath) {
-            cache.setOptions({ directory: options.cachePath });
+            cache.setOptions({
+                directory: options.cachePath
+            });
             delete options.cachePath;
         }
         delete options.cache;
@@ -31,6 +35,7 @@ function gulpPngquant(options, customPngquantPath) {
 
     // creating a stream through which each file will pass
     let stream = through.obj(function (file, enc, cb) {
+
         if (file.isStream()) {
             this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
             return cb();
@@ -43,25 +48,43 @@ function gulpPngquant(options, customPngquantPath) {
             return;
         }
 
-        const compressed = file.clone();
-
         if (file.isBuffer()) {
+            const pngquantBin = customPngquantPath || pngquant;
+
             console.log('compressing: ' + file.relative);
+            const compressed = file.clone();
 
-            compressed.contents = spawnSync(customPngquantPath || pngquant, opts, {
-                input: file.contents
-            }).stdout;
+            const child = spawn(pngquantBin, opts);
+            child.stdin.write(file.contents);
+            child.stdin.end();
 
-            if (cache) {
-                cache.set(file, compressed);
-            }
+            const buffers = [];
+            child.stdout.on("data", data => {
+                buffers.push(data);
+            });
+
+            child.on('exit', (code) => {
+                compressed.contents = Buffer.concat(buffers);
+                if (cache) {
+                    cache.set(file, compressed);
+                }
+                this.push(compressed);
+                cb();
+            });
+
+            // compressed.contents = spawnSync(customPngquantPath || pngquant, opts, {
+            //     input: file.contents
+            // }).stdout;
+            // if (cache) {
+            //     cache.set(file, compressed);
+            // }
+            // this.push(compressed);
+            // cb();
+
+        } else {
+            cb();
         }
 
-        // make sure the file goes through the next gulp plugin
-        this.push(compressed);
-
-        // tell the stream engine that we are done with this file
-        cb();
     }, function (flush) {
         log('Finished : ', PLUGIN_NAME);
         flush();
